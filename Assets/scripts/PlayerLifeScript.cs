3a097dfec13f46108ecd32569eb24184b3eca22c
brycebohling <96216764+brycebohling@users.noreply.github.com>
2022-12-29 16:40:18 -0800

    Working on player delay respawn have death effect but respawn now working

--- Assets/scripts/PlayerLifeScript.cs
+++ Assets/scripts/PlayerLifeScript.cs
@@ -7,21 +7,32 @@ public class PlayerLifeScript : MonoBehaviour
     [SerializeField] private Transform Player;
     public int whichCP = 0;
     private string checkPoint;
-
+    [SerializeField] ParticleSystem deathEffect;
+    [SerializeField] private float respawnCountdown;
 
 
     public void respawn()
     {
-        if (whichCP > 0)
-        {
-            checkPoint = "checkPoint" + whichCP;
+        StartCoroutine("respawnCoroutine");
+    }
+    private IEnumerator respawnCoroutine()
+    {
+        gameObject.SetActive(false);
 
-            Vector3 checkPointPosition = GameObject.FindGameObjectWithTag(checkPoint).transform.position;
+        deathEffect.gameObject.SetActive(true);
+        deathEffect.Stop();
+        deathEffect.transform.position = new Vector2(transform.position.x, transform.position.y);
+        deathEffect.Play();
 
-            Player.position = new Vector3(checkPointPosition.x, checkPointPosition.y + 1, Player.position.z);
-        } else
-        {
-            Player.position = new Vector3(0, -1, Player.position.z);
-        }
+        checkPoint = "checkPoint" + whichCP;
+
+        Vector3 checkPointPosition = GameObject.FindGameObjectWithTag(checkPoint).transform.position;
+
+        Player.position = new Vector3(checkPointPosition.x, checkPointPosition.y + 1, Player.position.z);
+
+        yield return new WaitForSeconds(respawnCountdown);
+        Debug.Log("finaly");
+        gameObject.SetActive(true);
+        
     }
 }
