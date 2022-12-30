b143fb47ac407eddf21c58641af6e4a6af0c452b
brycebohling <96216764+brycebohling@users.noreply.github.com>
2022-12-29 22:03:01 -0800

    working scaner killing the player

--- Assets/scripts/ScanArea.cs
+++ Assets/scripts/ScanArea.cs
@@ -4,19 +4,28 @@ using UnityEngine;
 
 public class ScanArea : MonoBehaviour
 {
+    GameMaster gm;
+    [SerializeField] private Transform player;    
     [SerializeField] private LineRenderer lineRenderer;
     [SerializeField] private float laserSpeed;
     private Quaternion originalRotation;
     private Quaternion minRotation;
     private Quaternion maxRotation;
-
+    [SerializeField] private float timeToKill;
+    private float timeToKillCountdown;
+    private bool onPlayer;
+    private float timer;
+    private float followPower = 100f;
 
     void Start()
     {
+        gm = GameObject.FindGameObjectWithTag("gameMaster").GetComponent<GameMaster>();
+
+        timeToKillCountdown = timeToKill;
         originalRotation = transform.rotation;
 
-        minRotation = originalRotation * Quaternion.Euler(0, 0, -100);
-        maxRotation = originalRotation * Quaternion.Euler(0, 0, -260);
+        minRotation = originalRotation * Quaternion.Euler(0, 0, -140);
+        maxRotation = originalRotation * Quaternion.Euler(0, 0, -220);
     }
 
     void Update()
@@ -26,11 +35,15 @@ public class ScanArea : MonoBehaviour
 
         lineRenderer.SetPosition(0, startPosition);
         
-        var factor = Mathf.PingPong(Time.time * laserSpeed, 1);
-
+        var factor = Mathf.PingPong(timer * laserSpeed, 1);
+        timer += Time.deltaTime;
         // instead of the eulers rather use Quaternion
-        transform.rotation = Quaternion.Lerp(minRotation, maxRotation, factor);
-
+        if (!onPlayer)
+        {
+            transform.rotation = Quaternion.Lerp(minRotation, maxRotation, factor);
+            
+        }
+        
         // "transform.up" basically equals using "transform.TransformDirection(Vector3.up)"
         var raycastHit2D = Physics2D.Raycast(startPosition, transform.up, 10f);
 
@@ -41,13 +54,29 @@ public class ScanArea : MonoBehaviour
 
             if (raycastHit2D.collider.gameObject.CompareTag("Player"))
             {
+                onPlayer = true;
+                Vector3 direction = player.position - transform.position;
+                // finding the  angle using invrse tan (have to -90 everytime because unity thing is always 90 off)
+                float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90;
+                // put the angle found into a axis, in this case it's the z axis
+                Quaternion angleAxis = Quaternion.AngleAxis(angle, Vector3.forward);
+                transform.rotation = Quaternion.Slerp(transform.rotation, angleAxis, followPower * Time.deltaTime);
+                // transform.eulerAngles = Vector3.forward * angle;
                 lineRenderer.startColor = Color.red;
                 lineRenderer.endColor = Color.red;
+
+                timeToKillCountdown -= Time.deltaTime;
+                if (timeToKillCountdown < 0)
+                {
+                    gm.killPlayer();
+                }
             }
             else 
             {
                 lineRenderer.startColor = Color.green;
                 lineRenderer.endColor = Color.green;
+                timeToKillCountdown = timeToKill;
+                onPlayer = false;
             }
         }
         else
@@ -56,6 +85,29 @@ public class ScanArea : MonoBehaviour
             lineRenderer.SetPosition(1, startPosition + transform.up * 10f);
             lineRenderer.startColor = Color.green;
             lineRenderer.endColor = Color.green;
+            timeToKillCountdown = timeToKill;
+            onPlayer = false;
         }  
     }
-}
+
+    private void lockedOnPlayer()
+    {
+        // finding the distance to the player
+        Vector3 direction = player.position - transform.position;
+        // finding the  angle using invrse tan (have to -90 everytime because unity thing is always 90 off)
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90;
+        // put the angle found into a axis, in this case it's the z axis
+        // Quaternion angleAxis = Quaternion.AngleAxis(angle, Vector3.forward);
+        // transform.rotation = Quaternion.Slerp(transform.rotation, angleAxis, laserSpeed * Time.deltaTime);
+        transform.eulerAngles = Vector3.forward * angle;
+        lineRenderer.startColor = Color.red;
+        lineRenderer.endColor = Color.red;
+
+        timeToKillCountdown -= Time.deltaTime;
+        if (timeToKillCountdown < 0)
+        {
+            gm.killPlayer();
+        }
+
+    }
+}   
