24cb4aa17fe857f39f9e96364ffc6916d0798c27
brycebohling <96216764+brycebohling@users.noreply.github.com>
2023-01-01 00:35:28 -0800

    working on levels

---
+++ Assets/scripts/SceneManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void playGame()
+    {
+        //  SceneManager.LoadScene("Mainscene");
+    }
+}
