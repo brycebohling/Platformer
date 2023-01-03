991fa844366584851db32375294eca0478d2aa60
brycebohling <96216764+brycebohling@users.noreply.github.com>
2023-01-02 18:16:11 -0800

    Working one bug with pausing and jumping

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -32,26 +32,12 @@ public class PlayerMovement : MonoBehaviour
     private bool wallJump;
     private bool wasTouchingWall;
 
-    //Dashing
-    // private bool canDash = true;
-    // private bool isDashing;
-    // private float dashingPower = 24f;
-    // private float dashingTime = 0.2f;
-    // private float dashingCooldown = 1f;
-    // [SerializeField] TrailRenderer tr;
-
     //Partical Effects
-
     [SerializeField] ParticleSystem jumpEffect;
 
 
     private void Update()
     {
-        // if (isDashing)
-        // {
-        //     return;
-        // }
-
         Flip();
 
         movementX = 0;
@@ -108,19 +94,10 @@ public class PlayerMovement : MonoBehaviour
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
         }
-
-        // if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
-        // {
-        //     StartCoroutine(Dash());
-        // }
     }
     
     private void FixedUpdate()
     {
-        // if (isDashing)
-        // {
-        //     return;
-        // }
         rb.velocity = new Vector2(movementX * speed, rb.velocity.y);
     }
 
@@ -137,22 +114,6 @@ public class PlayerMovement : MonoBehaviour
             transform.localScale = localScale;
         }
     }
-
-    // private IEnumerator Dash()
-    // {
-    //     canDash = false;
-    //     isDashing = true;
-    //     float originalGravity = rb.gravityScale;
-    //     rb.gravityScale = 0f;
-    //     rb.velocity = new Vector2(transform.localScale.x * dashingPower, 0f);
-    //     tr.emitting = true;
-    //     yield return new WaitForSeconds(dashingTime);
-    //     tr.emitting = false;
-    //     rb.gravityScale = originalGravity;
-    //     isDashing = false;
-    //     yield return new WaitForSeconds(dashingCooldown);
-    //     canDash = true;
-    // }
 
     private void jumpParticles()
     {
