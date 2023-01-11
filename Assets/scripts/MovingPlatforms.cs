d779fc531a594298f7618631cac9617c7fef6b96
brycebohling <96216764+brycebohling@users.noreply.github.com>
2023-01-10 21:16:05 -0800

    Saving before trying to build

--- Assets/scripts/MovingPlatforms.cs
+++ Assets/scripts/MovingPlatforms.cs
@@ -7,27 +7,28 @@ public class MovingPlatforms : MonoBehaviour
     [SerializeField] private Vector3 pos1;
     [SerializeField] private Vector3 pos2;
     [SerializeField] private float speed = 1.0f;
- 
+    [SerializeField] private Transform collisionToughChecker;
+    [SerializeField] private LayerMask playerLayer;
+    [SerializeField] private Transform player;
+    private bool toughingPlayer;
     
          
     void Update()
     {
+        toughingPlayer = Physics2D.OverlapBox(collisionToughChecker.position, new Vector2(1f, .3f), 0, playerLayer);
+
+        if (toughingPlayer)
+        {
+            player.transform.SetParent(transform);
+        } else 
+        {
+            player.transform.SetParent(null);
+        }
+        
+    }
+
+    void FixedUpdate()
+    {
         transform.position = Vector3.Lerp (pos1, pos2, Mathf.PingPong(Time.time * speed, 1.0f));
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player")) 
-        {
-            collision.transform.SetParent(transform);
-        }
-    }
-
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player")) 
-        {
-            collision.transform.SetParent(null);
-        }
     }
 }
